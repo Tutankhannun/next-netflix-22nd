16e2a2c508185eda419e656a4419b3b66e998f42
김윤성 <c335098@g.hongik.ac.kr>
2025-11-03 15:33:00 +0900

    design: 파비콘 추가

--- next-netflix-22nd/src/app/layout.tsx
+++ next-netflix-22nd/src/app/layout.tsx
@@ -1,6 +1,12 @@
 // my-app/src/app/layout.tsx
 import './globals.css';
+import type { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 export default function RootLayout({
   children,
 }: {
